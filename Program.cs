3a7cd7eef89ada17ffa9c5828af2b7480a0265e3
Teja Damineni <62346945+Teja-D222@users.noreply.github.com>
2025-12-02 12:00:24 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "about",
+    pattern: "about",
+    defaults: new { controller = "Home", action = "About" });
+
+app.MapControllerRoute(
+    name: "data",
+    pattern: "data",
+    defaults: new { controller = "Home", action = "Read" });
+
+app.MapControllerRoute(
+    name: "advisor",
+    pattern: "advisor",
+    defaults: new { controller = "Home", action = "SatForm" });
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
